c3538a4010604ef920154245fe4820ef053bacc7
David Bernal <david.3pl@gmail.com>
2021-02-20 10:55:54 -0500

    eg1

---
+++ a_NODE18S_C1U1L2_Screencast1_NPM_V1/codigo_final/ejemplo.js
@@ -0,0 +1,10 @@
+var n2f = require('num2fraction')
+//var sumar = require('./mate')
+
+function sumar(a,b){
+  return a + b
+}
+
+console.log(sumar(2,4));
+console.log(sumar(2,9));
+console.log(n2f(1.85))
---
+++ c_NODE18S_C1U1L2_EjercicioSolucion1_V1/es-numero.js
@@ -0,0 +1,12 @@
+var isNumber = require('is-number')
+
+function esNumero(expresion){
+  if (isNumber(expresion)){
+    return "Es un número"
+  }
+  return "No es un número"
+}
+
+module.exports = esNumero
+
+
